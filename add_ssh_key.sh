1321dec2fc2b7d6583ad904c62e19f655aa8fac5
apros7 <lucas.vilsen@gmail.com>
2025-09-27 10:39:43 +0200

    easy to add ssh key

---
+++ add_ssh_key.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Change to your ssh key
+SSH_KEY="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIFcJCEv4bHApgfmbuEb9MOlEMsusfke3CQUjXuKGgn5 my-laptop"
+
+echo "$SSH_KEY" >> ~/.ssh/authorized_keys
+
+if ! grep -q "$SSH_KEY" ~/.ssh/authorized_keys; then
+    echo "SSH key not found in authorized_keys"
+    exit 1
+fi
+
+echo "SSH key added to authorized_keys"
+exit 0
