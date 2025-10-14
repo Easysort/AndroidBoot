aee4f3b356ed527d81c4a9754c04623b0d985154
apros7 <lucas.vilsen@gmail.com>
2025-10-14 16:49:07 +0200

    cleanup, delete files

--- run.sh
+++ run.sh
@@ -1,1 +1,19 @@
-termux-job-scheduler --job-id 700 --script "$HOME/AndroidBoot/tmux-starter.sh" --period-ms 900000 --persisted true --network any --battery-not-low false
+#!/bin/bash
+
+show_usage() {
+  echo "Usage: $0 -a|--applications <app> \n <app> can be: container, entrance"; exit 1
+}
+
+[ "$1" = "-a" ] || [ "$1" = "--applications" ] && APP="$2" || show_usage
+
+if [ -z "$APP" ]; then
+    show_usage
+fi
+
+case "$APP" in
+  container) SCRIPT_PATH="$HOME/AndroidBoot/applications/container/tmux-starter.sh" ;;
+  entrance) SCRIPT_PATH="$HOME/AndroidBoot/applications/entrance/tmux-starter.sh" ;;
+  *) echo "Unknown application: $APP"; show_usage ;;
+esac
+
+termux-job-scheduler --job-id 700 --script "$SCRIPT_PATH" --period-ms 900000 --persisted true --network any --battery-not-low false
